2abfb24646c24a89bba22ea7b6a261a048a81ff5
Jon Scheiding <jonscheiding@gmail.com>
2022-01-20 09:18:41 -0500

    Refactor and add most of the content

---
+++ components/PresentCard.js
@@ -0,0 +1,105 @@
+import { useState } from "react";
+
+export default function PresentCard({description, recipient, children}) {
+  const [isUnwrapped, setIsUnwrapped] = useState(false);
+  const [showingDetails, setShowingDetails] = useState(false);
+  
+  const key = description.replace(/[^A-Za-z0-9]/g, '-').toLowerCase()
+  const imgSrc = `/images/${key}-${isUnwrapped ? 'unwrapped' : 'wrapped'}.jpg`
+
+  function cardClicked() {
+    setShowingDetails(true);
+  }
+
+  function unwrapClicked() {
+    setIsUnwrapped(true)
+  }
+  
+  function closeClicked() {
+    setShowingDetails(false)
+  }
+
+  return (
+    <>
+      <div 
+        className="modal"
+        style={{display: showingDetails ? 'block' : 'none'}}
+        onClick={closeClicked}>
+        <div className="card" onClick={(e) => {e.stopPropagation()}}>
+          <img src={imgSrc} />
+          {
+            isUnwrapped
+              ? <>
+                  <h3>{description}</h3>
+                  <i>for {recipient}</i>
+                  {children}
+                  <button className="flat" onClick={closeClicked}>Thanks</button>
+                </>
+              : <>
+                  <i>for {recipient}</i>
+                  <h3>Unwrap this present?</h3>
+                  <button className="flat" onClick={unwrapClicked}>I did</button>
+                </>
+          }
+        </div>
+      </div>
+      <button className="card" onClick={cardClicked}>
+        <img src={imgSrc} />
+      </button>
+      <style jsx>{`
+        .modal {
+          top: 0;
+          left: 0;
+          position: fixed;
+          background-color: white;
+          width: 100vw;
+          min-height: 100vh;
+          background-color: rgba(0, 0, 0, 0.5);
+          padding: 1rem;
+        }
+
+        .modal .card {
+          background-color: white;
+          max-width: 480px;
+          margin: auto;
+        }
+
+        .modal i {
+          font-size: 1.2rem;
+          margin: 0.5rem;
+          display: block;
+        }
+
+        .card {
+          margin: 1rem;
+          flex-basis: 45%;
+          padding: 1.5rem;
+          text-align: left;
+          color: inherit;
+          text-decoration: none;
+          border: 1px solid #eaeaea;
+          border-radius: 10px;
+          transition: color 0.15s ease, border-color 0.15s ease;
+          text-align: center;
+        }
+
+        .card h3 {
+          margin: 0 0 1rem 0;
+          font-size: 1.5rem;
+          text-transform: uppercase;
+        }
+
+        .card p {
+          margin: 0;
+          font-size: 1.25rem;
+          line-height: 1.5;
+        }
+
+        .card img {
+          width: 100%;
+          display: block;
+        }      
+      `}</style>
+    </>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,105 +1,7 @@
 import Head from 'next/head'
-import { useState } from 'react'
-
-const presents = [
-  { wrappedImage: '/images/kraft-package.jpg', unwrappedImage: '/images/unwrapped.jpg', summary: 'A cool shirt', description: 'Not sure what else to say about this' },
-  { wrappedImage: '/images/kraft-package.jpg', unwrappedImage: '/images/unwrapped.jpg', summary: 'A cool bike lock', description: 'Make sure you use it' }
-]
+import PresentCard from '../components/PresentCard';
 
 export default function Home() {
-  function Present({presentInitial}) {
-    const [present, setPresent] = useState(presentInitial);
-    const [modal, setModal] = useState(false);
-
-    function unwrap() {
-      setPresent({
-        ...present,
-        isUnwrapped: true
-      })
-    }
-
-    function openModal() {
-      setModal(true);
-    }
-
-    function closeModal() {
-      setModal(false);
-    }
-
-    return (
-      <>
-        {modal ? 
-          <div className="modal card">
-            {present.isUnwrapped
-              ? <div>
-                  <img src={present.unwrappedImage} />
-                  <h3>{present.summary}</h3>
-                  <p>{present.description}</p>
-                  <button onClick={closeModal}>THANKS</button>
-                </div>
-              : <div>
-                  <img src={present.wrappedImage} />
-                  <h3>Unwrap this present</h3>
-                  <button onClick={unwrap}>I DID</button>
-                </div>
-            }
-          </div>
-        : null}
-        <button className="card" onClick={openModal}>
-          <img src={present.isUnwrapped ? present.unwrappedImage : present.wrappedImage} />
-        </button>
-        <style jsx>{`
-          button {
-            background-color: inherit;
-          }
-
-          .modal {
-            top: 0;
-            left: 0;
-            position: fixed;
-            width: calc(100vw - 2rem);
-            min-height: calc(100vh - 2rem);
-            background-color: white;
-          }
-
-          .card {
-            margin: 1rem;
-            flex-basis: 45%;
-            padding: 1.5rem;
-            text-align: left;
-            color: inherit;
-            text-decoration: none;
-            border: 1px solid #eaeaea;
-            border-radius: 10px;
-            transition: color 0.15s ease, border-color 0.15s ease;
-          }
-
-          .card:hover,
-          .card:focus,
-          .card:active {
-            color: #0070f3;
-            border-color: #0070f3;
-          }
-
-          .card h3 {
-            margin: 0 0 1rem 0;
-            font-size: 1.5rem;
-          }
-
-          .card p {
-            margin: 0;
-            font-size: 1.25rem;
-            line-height: 1.5;
-          }
-
-          .card img {
-            width: 100%;
-          }
-        `}</style>
-      </>
-    )
-  }
-
   return (
     <div className="container">
       <Head>
@@ -117,9 +19,67 @@ export default function Home() {
         </p>
 
         <div className="grid">
-          {presents.map(present => (
-            <Present presentInitial={present} />
-          ))}
+          <PresentCard description="Litographs Pride Shirt" recipient="Ashley">
+            <p>
+              This shirt is printed with the text of the Obergefell v. Hodges Supreme Court decision. 
+              It's from a website called <a href="https://litographs.com" target="_blank">Litographs</a> that
+              does this with all kinds of text sources.
+            </p>
+            <p>
+              If it's the wrong size, let me know, I can exchange it.
+            </p>
+            <p>
+              Full disclosure, I liked it so much when I saw it that I got one for myself too. Sorry for 
+              twinning.
+            </p>
+          </PresentCard>
+          <PresentCard description="Metal Space Models" recipient="Ashley">
+            <p>
+              These are some models made out of metal. You assemble them by just punching out the perforated
+              pieces from the sheet and then snapping them together. Presumably there are some instructions.
+            </p>
+            <p>
+              I wasn't sure if you'd like doing these, but I thought maybe it would be fun to work on them 
+              together next time we hang out. Or, if you don't like actually putting them together, I can do
+              them for you.
+            </p>
+          </PresentCard>
+          <PresentCard description="Beat BG Button" recipient="Chas">
+            <p>
+              I couldn't stand how sad Chas was that he couldn't find this in the M Den when I was there,
+              so I made one. Way to show your school spirit.
+            </p>
+          </PresentCard>
+          <PresentCard description="San Pellegrino Chocolate Coffee" recipient="Grace">
+            <p>
+              "There's something so human about taking something great and ruining it a little so you can
+              have more of it." I know you love chocolate and coffee, so I couldn't resist giving you some
+              of these drinks that sort of ruin them both.
+            </p>
+            <p>
+              Actually I really like these, but if you don't, well that's what makes it a joke gift.
+            </p>
+          </PresentCard>
+          <PresentCard description="Odegra Poster" recipient="Ashley">
+            <p>
+              This isn't really a present; I had it hanging up in the room you'd have been in when you
+              were going to come visit. But now I have to bring the trolling to you.
+            </p>
+            <p>
+              If you don't want this, I will legit hang it up in my house.
+            </p>
+          </PresentCard>
+          <PresentCard description="Bike Lock" recipient="Ashley">
+            <p>
+              This is a fairly heavy-duty lock, so hopefully it will keep your next bike from getting stolen.
+            </p>
+          </PresentCard>
+          <PresentCard description="Hot Chocolate Party" recipient="Whoever">
+            <p>
+              I know you're not that into sweets and maybe Grace or Chas will like this more than you
+              will. But you deserve marshmallows too, and luckily there is Trader Joe's.
+            </p>
+          </PresentCard>
         </div>
       </main>
 
@@ -142,13 +102,12 @@ export default function Home() {
           align-items: center;
         }
 
-        a {
-          color: inherit;
-          text-decoration: none;
+        a, a:hover, a:active, a:visited {
+          color: blue;
         }
 
-        button {
-          background-color: inherit;
+        p {
+          text-align: justify;
         }
 
         .title {
@@ -193,6 +152,10 @@ export default function Home() {
           font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
             Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
             sans-serif;
+        }
+
+        button { 
+          background: none;
         }
 
         * {
