398f0c8ab552b8ced26df630c8ebacafe98c6a31
Jon Scheiding <jonscheiding@gmail.com>
2022-01-19 21:04:37 -0500

    Initial POC

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,115 @@
 import Head from 'next/head'
+import { useState } from 'react'
+
+const presents = [
+  { wrappedImage: '/images/kraft-package.jpg', unwrappedImage: '/images/unwrapped.jpg', summary: 'A cool shirt', description: 'Not sure what else to say about this' },
+  { wrappedImage: '/images/kraft-package.jpg', unwrappedImage: '/images/unwrapped.jpg', summary: 'A cool bike lock', description: 'Make sure you use it' }
+]
 
 export default function Home() {
+  function Present({presentInitial}) {
+    const [present, setPresent] = useState(presentInitial);
+    const [modal, setModal] = useState(false);
+
+    function unwrap() {
+      setPresent({
+        ...present,
+        isUnwrapped: true
+      })
+    }
+
+    function openModal() {
+      setModal(true);
+    }
+
+    function closeModal() {
+      setModal(false);
+    }
+
+    return (
+      <>
+        {modal ? 
+          <div className="modal card">
+            {present.isUnwrapped
+              ? <div>
+                  <img src={present.unwrappedImage} />
+                  <h3>{present.summary}</h3>
+                  <p>{present.description}</p>
+                  <button onClick={closeModal}>THANKS</button>
+                </div>
+              : <div>
+                  <img src={present.wrappedImage} />
+                  <h3>Unwrap this present</h3>
+                  <button onClick={unwrap}>I DID</button>
+                </div>
+            }
+          </div>
+        : null}
+        <button className="card" onClick={openModal}>
+          <img src={present.isUnwrapped ? present.unwrappedImage : present.wrappedImage} />
+        </button>
+        <style jsx>{`
+          button {
+            background-color: inherit;
+          }
+
+          .modal {
+            top: 0;
+            left: 0;
+            position: fixed;
+            width: calc(100vw - 2rem);
+            min-height: calc(100vh - 2rem);
+            background-color: white;
+          }
+
+          .card {
+            margin: 1rem;
+            flex-basis: 45%;
+            padding: 1.5rem;
+            text-align: left;
+            color: inherit;
+            text-decoration: none;
+            border: 1px solid #eaeaea;
+            border-radius: 10px;
+            transition: color 0.15s ease, border-color 0.15s ease;
+          }
+
+          .card:hover,
+          .card:focus,
+          .card:active {
+            color: #0070f3;
+            border-color: #0070f3;
+          }
+
+          .card h3 {
+            margin: 0 0 1rem 0;
+            font-size: 1.5rem;
+          }
+
+          .card p {
+            margin: 0;
+            font-size: 1.25rem;
+            line-height: 1.5;
+          }
+
+          .card img {
+            width: 100%;
+          }
+        `}</style>
+      </>
+    )
+  }
+
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Christmas</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          XMAS
         </h1>
 
         <p className="description">
@@ -18,46 +117,11 @@ export default function Home() {
         </p>
 
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {presents.map(present => (
+            <Present presentInitial={present} />
+          ))}
         </div>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
 
       <style jsx>{`
         .container {
@@ -78,39 +142,13 @@ export default function Home() {
           align-items: center;
         }
 
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
         a {
           color: inherit;
           text-decoration: none;
         }
 
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
+        button {
+          background-color: inherit;
         }
 
         .title {
@@ -129,15 +167,6 @@ export default function Home() {
           font-size: 1.5rem;
         }
 
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
         .grid {
           display: flex;
           align-items: center;
@@ -146,40 +175,6 @@ export default function Home() {
 
           max-width: 800px;
           margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
         }
 
         @media (max-width: 600px) {
